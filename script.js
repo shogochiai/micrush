452e93511de53e68a8088a452934cfdc070f4d9a
@sgtn <shogo1104@gmail.com>
2014-10-19 00:24:26 +0900

    upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var appid = "io-fi1eagrco";
 var milkcocoa = new MilkCocoa("https://"+appid+".mlkcca.com");
 var ds_crush = milkcocoa.dataStore("crush");
+var ds_pics = milkcocoa.dataStore("pics");
 
 /* セリフ定義 */
 var serifs = [
@@ -14,15 +15,15 @@ var serifs = [
     "ぐっ・・・生き返るのも大変なのよっ！",
     "いっ・・たぁ〜！",
     "ふええええええ＞＜"
-]
+];
 
 /* 崩壊共有 */
-ds_crush.on("send", function(e){ $(document).trigger('click'); });
 ds_crush.on("push", function(e){
+    $(document).trigger('click');
     ds_crush.query({}).done(function(e){
-        var random_serif_num = Math.floor(Math.random()*serifs.length);
+        /* コメント＆回数更新 */
         setTimeout(function(){
-            $("p#comment").html(serifs[random_serif_num]);
+            $("p#comment").html(serifs[rand(0, serifs.length)]);
             $("p#num").html(e.length+"発目");
         }, 1600);
     })
@@ -35,19 +36,59 @@ window.chatpart.start({
     milkcocoa : milkcocoa
 });
 
-$("div.content").ready(function() {
+/* 画像アップロード */
+ds_pics.on("send", function(e){
+  $("img#miku").attr("src", e.value.bin);
+});
+
+/* 画像を取得し埋め込み */
+$("input#upload").click(function(e){
+  e.stopPropagation();
+});
+
+$("input#upload").change(function(e){
+    var file = e.target.files[0]
+    if(file) readfile(file);
+});
+
+function readfile(file) {
+    var reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = loaded;
+    reader.onerror = error;
+}
+
+function loaded(e){
+    var str = e.target.result;
+    ds_pics.send({"bin":str});
+}
+
+function error(){
+    alert("ERROR");
+}
+
+
+$("document").ready(function() {
     ds_crush.query({}).done(function(e){
         var count = e.length;
         $("#num").append(count+"発目");
+
+        /* コピー生成 */
         (genClips = function() {
             $t = $('.clipped-box');
+
+            /* 破片の定義 */
             var amount = 10;
             var width = $t.width() / amount;
             var height = $t.height() / amount;
             var totalSquares = Math.pow(amount, 2);
             var html = $t.find('.content').html();
             var y = 0;
+
+            /* 破片生成 */
             for(var z = 0; z <= (amount*width); z = z+width) {
+
+                /* clip: rect でコピー */
                 $('<div class="clipped" style="clip: rect('+y+'px, '+(z+width)+'px, '+(y+height)+'px, '+z+'px)">'+html+'</div>').appendTo($t);
 
                 if(z === (amount*width)-width) {
@@ -59,20 +100,21 @@ $("div.content").ready(function() {
         })();
     });
 
-    function rand(min, max) {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    var first = false;
+    var clicked = false;
 
-    var first = false,
-    clicked = false;
-
+    /* クリック時動作 */
     $(document).on('click', function() {
         if(clicked === false) {
             clicked = true;
-            ds_crush.send({"hoge":"hoge"});
+
+            /* 他ユーザーに崩壊共有 */
             ds_crush.push({"hoge":"hoge"});
 
+            /* コピー元を不可視に */
             $('.clipped-box .content').css({'display' : 'none'});
+
+            /* 動きを定義 */
             $('.clipped-box div:not(.content)').each(function() {
                 var v = rand(120, 90),
                 angle = rand(80, 89),
@@ -122,3 +164,7 @@ $("div.content").ready(function() {
         }
     }, 300);
 });
+
+function rand(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
